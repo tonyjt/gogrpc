54c665a19a139ad8c33722cd0813208d798706e6
tonyjt <tonyliujiangtao@gmail.com>
2017-01-09 15:20:47 +0800

    balance ip finished

--- balancer_ip.go
+++ balancer_ip.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	//"log"
 	"math/rand"
 	"sync"
+	"time"
 )
 
 type BalancerIp struct {
@@ -52,60 +54,78 @@ func (b *BalancerIp) Start(target string, config grpc.BalancerConfig) error {
 // TODO: It is not clear how to construct and take advantage of the meaningful error
 // parameter for down. Need realistic demands to guide.
 func (b *BalancerIp) Up(addr grpc.Address) (down func(error)) {
-
-	b.mu.Lock()
+	//log.Printf("up :addr:%s\n", addr.Addr)
+	//b.mu.Lock()
 	for i, a := range b.addrDown {
 		if addr.Addr == a.Addr {
 			b.addrDown = append(b.addrDown[:i], b.addrDown[i+1:]...)
 			break
 		}
 	}
-	for i := 0; i < len(b.addrUp); i++ {
-		if b.addrUp[i].Addr == addr.Addr {
-			break
-		}
 
-		if i == len(b.addrUp)-1 {
-			b.addrUp = append(b.addrUp, addr)
+	for i, a := range b.addr {
+		if addr.Addr == a.Addr {
+			b.addr = append(b.addr[:i], b.addr[i+1:]...)
 			break
 		}
 	}
-
-	b.mu.Unlock()
-	return func(error) {
-		b.mu.Lock()
+	var iUp int
+	for iUp = 0; iUp < len(b.addrUp); iUp++ {
+		if b.addrUp[iUp].Addr == addr.Addr {
+			break
+		}
+	}
+	if iUp >= len(b.addrUp) {
+		b.addrUp = append(b.addrUp, addr)
+	}
+	address := addr.Addr
+	//b.mu.Unlock()
+	down = func(err error) {
+		//log.Printf("down :addr:%s,err:%s \n", address, err.Error())
+		//b.mu.Lock()
 		for i, a := range b.addrUp {
-			if addr.Addr == a.Addr {
+			if address == a.Addr {
 				b.addrUp = append(b.addrUp[:i], b.addrUp[i+1:]...)
 				break
 			}
 		}
-		for i := 0; i < len(b.addrDown); i++ {
-			if b.addrDown[i].Addr == addr.Addr {
-				break
-			}
 
-			if i == len(b.addrDown)-1 {
-				b.addrUp = append(b.addrDown, addr)
+		var iDown int
+		for iDown = 0; iDown < len(b.addrDown); iDown++ {
+			if b.addrDown[iDown].Addr == address {
 				break
 			}
 		}
-
-		b.mu.Unlock()
+		if iDown >= len(b.addrDown) {
+			b.addrDown = append(b.addrDown, addr)
+		}
+		//b.mu.Unlock()
 	}
+	return
 }
 
 func (b *BalancerIp) Get(ctx context.Context, opts grpc.BalancerGetOptions) (addr grpc.Address, put func(), err error) {
-	if len(b.addrUp) == 0 {
+
+	//log.Println("Get")
+	if len(b.addr) == 0 && len(b.addrUp) == 0 {
 		err = errors.New("all addr is down")
-
-	} else if len(b.addrUp) == 1 {
-		addr = b.addrUp[0]
-
 	} else {
-		index := rand.Intn(len(b.addrUp))
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		var index int
+
+		if len(b.addr) > 0 {
+			index = rand.Intn(len(b.addr))
+			//log.Printf("len addr: %d,index :%d\n", len(b.addr), index)
+			addr = b.addr[index]
+
+		} else {
+			index = rand.Intn(len(b.addrUp))
+			//log.Printf("len up :%d,index :%d\n", len(b.addrUp), index)
+			addr = b.addrUp[index]
+		}
+
 		put = nil
-		addr = b.addrUp[index]
 
 	}
 	return
@@ -118,7 +138,7 @@ func (b *BalancerIp) Notify() <-chan []grpc.Address {
 func (b *BalancerIp) Close() error {
 	close(b.chanNotify)
 	b.addr = nil
-	b.addrUp = nil
 	b.addrDown = nil
+	b.addrUp = nil
 	return nil
 }
